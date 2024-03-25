3823abea93e6fa2291f4987544197058d854e770
coderdevi1 <101414336+coderdevi1@users.noreply.github.com>
2024-03-25 12:59:48 +1100

    Add project files.

---
+++ source.cpp
@@ -0,0 +1,12 @@
+/*
+Task 1: Reward Point
+Writing a algorithm that will enable users to enter the distance within a range from 0-40,
+which calculates the percentage and gives appropriate grade for the following percentage.
+Step 1: Start
+Step 2: Declare distance, percentage and rewardPoints as variables
+Step 3: Prompt user to enter a distance from 0 to 40
+Step 4: If distance is lower than 0 print invalid value
+Step 5:
+Step 6:
+Step 7:
+*/
